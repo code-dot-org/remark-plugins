ac974cd74de25adc44232aea140ccafbc2789591
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-06-21 16:49:20 -0700

    import all plugins from CurriculumBuilder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const blockfield = require('./blockfield');
 const blockly = require('./blockly');
 const details = require('./details');
+const resourcelink = require('./resourcelink');
+const tip = require('./tip');
+const tiplink = require('./tiplink');
+const vocablink = require('./vocablink');
 
 module.exports = {
   blockfield,
   blockly,
-  details
+  details,
+  resourcelink,
+  tip,
+  tiplink,
+  vocablink
 };

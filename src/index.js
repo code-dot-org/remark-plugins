2ced578b7a1b272a6adf916ca1c7a968873de508
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-06-21 17:09:07 -0700

    add tests for tip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const blockfield = require('./blockfield');
 const blockly = require('./blockly');
 const details = require('./details');
+const indent = require('./indent');
 const resourcelink = require('./resourcelink');
 const tip = require('./tip');
 const tiplink = require('./tiplink');
@@ -10,6 +11,7 @@ module.exports = {
   blockfield,
   blockly,
   details,
+  indent,
   resourcelink,
   tip,
   tiplink,

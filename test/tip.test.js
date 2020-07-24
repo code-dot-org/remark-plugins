4f6d6e38ba1455a36cafd91ac7340ec2d7a2cc2c
hacodeorg <ha@code.org>
2020-07-24 12:31:00 -0700

    update expected outputs of the first 7 tests

--- test/tip.test.js
+++ test/tip.test.js
@@ -14,20 +14,14 @@ test("tip plugin renders a basic tip", t => {
   const input =
     '!!!tip "this is an optional title, and it should be translatable" <tip-0>\n    This is the content of the tip, and it should be translatable\n    This is more stuff that is still part of the content of the tip\n\nThis is the next paragraph';
   const output = markdownToHtml(input, tip);
-  /**
-   * <div class="admonition tip">
-   *   <p class="admonition-title" id="tip_tip-0">
-   *     <i class="fa fa-lightbulb-o"></i>
-   *     this is an optional title, and it should be translatable
-   *   </p>
-   *   <p>
-   *     This is the content of the tip, and it should be translatable This is more stuff that is still part of the content of the tip
-   *   </p>
-   * </div>
-   * <p>This is the next paragraph</p>
-   */
   const expected =
-    '<div class="admonition tip"><p class="admonition-title" id="tip_tip-0"><i class="fa fa-lightbulb-o"></i>this is an optional title, and it should be translatable</p><p>This is the content of the tip, and it should be translatable\nThis is more stuff that is still part of the content of the tip</p></div>\n<p>This is the next paragraph</p>\n';
+    '<div class="admonition tip">' +
+        '<p class="admonition-title" id="tip_tip-0"><i class="fa fa-lightbulb-o"></i>this is an optional title, and it should be translatable</p>' +
+        '<div>' +
+            '<p>This is the content of the tip, and it should be translatable\nThis is more stuff that is still part of the content of the tip</p>' +
+        '</div>' +
+    '</div>\n' +
+    '<p>This is the next paragraph</p>\n';
   t.equal(output, expected);
 });
 
@@ -36,21 +30,14 @@ test("tip plugin renders a basic tip even with weird indentation", t => {
   const input =
     '!!!tip "this is an optional title, and it should be translatable" <tip-0>\n\tThis is the content of the tip, and it should be translatable, as should all the following blocks:\n \tone\n\t\t\t\ttwo\n \t three\n              four\n\nThis is the next paragraph';
   const output = markdownToHtml(input, tip);
-  /**
-   * <div class="admonition tip">
-   *   <p class="admonition-title" id="tip_tip-0">
-   *     <i class="fa fa-lightbulb-o"></i>
-   *     this is an optional title, and it should be translatable
-   *   </p>
-   *   <p>
-   *     This is the content of the tip, and it should be translatable, as
-   *     should all the following blocks: one two three four
-   *   </p>
-   * </div>
-   * <p>This is the next paragraph</p>
-   */
   const expected =
-    '<div class="admonition tip"><p class="admonition-title" id="tip_tip-0"><i class="fa fa-lightbulb-o"></i>this is an optional title, and it should be translatable</p><p>This is the content of the tip, and it should be translatable, as should all the following blocks:\none\ntwo\nthree\nfour</p></div>\n<p>This is the next paragraph</p>\n';
+    '<div class="admonition tip">' +
+        '<p class="admonition-title" id="tip_tip-0"><i class="fa fa-lightbulb-o"></i>this is an optional title, and it should be translatable</p>' +
+        '<div>' +
+            '<p>This is the content of the tip, and it should be translatable, as should all the following blocks:\none\ntwo\nthree\nfour</p>' +
+        '</div>' +
+    '</div>\n' +
+    '<p>This is the next paragraph</p>\n';
   t.equal(output, expected);
 });
 
@@ -59,21 +46,14 @@ test("tip plugin renders a basic tip without an id", t => {
   const input =
     '!!!tip "this is an optional title, and it should be translatable"\n    This is the content of the tip, and it should be translatable\n    This is more stuff that is still part of the content of the tip\n\nThis is the next paragraph';
   const output = markdownToHtml(input, tip);
-  /**
-   * <div class="admonition tip">
-   *   <p class="admonition-title" id="tip_None">
-   *     <i class="fa fa-lightbulb-o"></i>
-   *     this is an optional title, and it should be translatable
-   *   </p>
-   *   <p>
-   *     This is the content of the tip, and it should be translatable
-   *     This is more stuff that is still part of the content of the tip
-   *   </p>
-   * </div>
-   * <p>This is the next paragraph</p>
-   */
   const expected =
-    '<div class="admonition tip"><p class="admonition-title" id="tip_None"><i class="fa fa-lightbulb-o"></i>this is an optional title, and it should be translatable</p><p>This is the content of the tip, and it should be translatable\nThis is more stuff that is still part of the content of the tip</p></div>\n<p>This is the next paragraph</p>\n';
+    '<div class="admonition tip">' +
+        '<p class="admonition-title" id="tip_None"><i class="fa fa-lightbulb-o"></i>this is an optional title, and it should be translatable</p>' +
+        '<div>' +
+            '<p>This is the content of the tip, and it should be translatable\nThis is more stuff that is still part of the content of the tip</p>' +
+        '</div>' +
+    '</div>\n' +
+    '<p>This is the next paragraph</p>\n';
   t.equal(output, expected);
 });
 
@@ -82,21 +62,14 @@ test("tip plugin renders a basic tip without a title", t => {
   const input =
     "!!!tip <tip-0>\n    This is the content of the tip, and it should be translatable\n    This is more stuff that is still part of the content of the tip\n\nThis is the next paragraph";
   const output = markdownToHtml(input, tip);
-  /**
-   * <div class="admonition tip">
-   *   <p class="admonition-title" id="tip_tip-0">
-   *     <i class="fa fa-lightbulb-o"></i>
-   *     Teaching Tip
-   *   </p>
-   *   <p>
-   *     This is the content of the tip, and it should be translatable
-   *     This is more stuff that is still part of the content of the tip
-   *   </p>
-   * </div>
-   * <p>This is the next paragraph</p>
-   */
   const expected =
-    '<div class="admonition tip"><p class="admonition-title" id="tip_tip-0"><i class="fa fa-lightbulb-o"></i>Teaching Tip</p><p>This is the content of the tip, and it should be translatable\nThis is more stuff that is still part of the content of the tip</p></div>\n<p>This is the next paragraph</p>\n';
+      '<div class="admonition tip">' +
+          '<p class="admonition-title" id="tip_tip-0"><i class="fa fa-lightbulb-o"></i>Teaching Tip</p>' +
+          '<div>' +
+              '<p>This is the content of the tip, and it should be translatable\nThis is more stuff that is still part of the content of the tip</p>' +
+          '</div>' +
+      '</div>\n' +
+      '<p>This is the next paragraph</p>\n';
   t.equal(output, expected);
 });
 
@@ -105,23 +78,17 @@ test("tip plugin renders a tip with multiple children", t => {
   const input =
     '!!!tip "this is an optional title, and it should be translatable" <tip-0>\n    This is the content of the tip, and it should be translatable\n\n    This is more stuff that is still part of the content of the tip\n\nThis is the next paragraph';
   const output = markdownToHtml(input, tip);
-  /**
-   * <div class="admonition tip">
-   *   <p class="admonition-title" id="tip_tip-0">
-   *     <i class="fa fa-lightbulb-o"></i>
-   *     this is an optional title, and it should be translatable
-   *   </p>
-   *   <p>
-   *     This is the content of the tip, and it should be translatable
-   *   </p>
-   *   <p>
-   *     This is more stuff that is still part of the content of the tip
-   *   </p>
-   * </div>
-   * <p>This is the next paragraph</p>
-   */
   const expected =
-    '<div class="admonition tip"><p class="admonition-title" id="tip_tip-0"><i class="fa fa-lightbulb-o"></i>this is an optional title, and it should be translatable</p><p>This is the content of the tip, and it should be translatable</p><p>This is more stuff that is still part of the content of the tip</p></div>\n<p>This is the next paragraph</p>\n';
+      '<div class="admonition tip">' +
+          '<p class="admonition-title" id="tip_tip-0"><i class="fa fa-lightbulb-o"></i>this is an optional title, and it should be translatable</p>' +
+          '<div>' +
+              '<p>This is the content of the tip, and it should be translatable</p>' +
+          '</div>' +
+          '<div>' +
+              '<p>This is more stuff that is still part of the content of the tip</p>' +
+          '</div>' +
+      '</div>\n' +
+      '<p>This is the next paragraph</p>\n';
   t.equal(output, expected);
 });
 
@@ -130,21 +97,14 @@ test("tip plugin renders a basic tip indented with tabs", t => {
   const input =
     '!!!tip "this is an optional title, and it should be translatable" <tip-0>\n\tThis is the content of the tip, and it should be translatable\n\tThis is more stuff that is still part of the content of the tip\n\nThis is the next paragraph';
   const output = markdownToHtml(input, tip);
-  /*
-    <div class="admonition tip">
-      <p class="admonition-title" id="tip_tip-0">
-        <i class="fa fa-lightbulb-o"></i>
-        this is an optional title, and it should be translatable
-      </p>
-      <p>
-        This is the content of the tip, and it should be translatable
-        This is more stuff that is still part of the content of the tip
-      </p>
-    </div>
-    <p>This is the next paragraph</p>
-  */
   const expected =
-    '<div class="admonition tip"><p class="admonition-title" id="tip_tip-0"><i class="fa fa-lightbulb-o"></i>this is an optional title, and it should be translatable</p><p>This is the content of the tip, and it should be translatable\nThis is more stuff that is still part of the content of the tip</p></div>\n<p>This is the next paragraph</p>\n';
+      '<div class="admonition tip">' +
+          '<p class="admonition-title" id="tip_tip-0"><i class="fa fa-lightbulb-o"></i>this is an optional title, and it should be translatable</p>' +
+          '<div>' +
+              '<p>This is the content of the tip, and it should be translatable\nThis is more stuff that is still part of the content of the tip</p>' +
+          '</div>' +
+      '</div>\n' +
+      '<p>This is the next paragraph</p>\n';
   t.equal(output, expected);
 });
 
@@ -159,7 +119,7 @@ test("tip plugin does not render title paragraph unless there is a title", t => 
   t.plan(1);
   const input = "!!!guide <content-0>\n\n\tinner content";
   const output = markdownToHtml(input, tip);
-  const expected = '<div class="admonition guide"><p>inner content</p></div>\n';
+  const expected = '<div class="admonition guide"><div></div><div><p>inner content</p></div></div>\n';
   t.equal(output, expected);
 });
 
